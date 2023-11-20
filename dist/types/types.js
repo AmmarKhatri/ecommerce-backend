05413ed5edadbb5e753b10176ccb7d04804d4f65
Ammar Khatri <43322268+AmmarKhatri@users.noreply.github.com>
2023-11-20 18:13:08 +0500

    repo modularized

--- dist/types/types.js
+++ dist/types/types.js
@@ -1,9 +1,4 @@
+import { books } from './books.js';
 export const typeDefs = `#graphql
-  type Book {
-    title: String
-    author: String
-  }
-  type Query {
-    books: [Book]
-  }
+  ${books}
 `;
